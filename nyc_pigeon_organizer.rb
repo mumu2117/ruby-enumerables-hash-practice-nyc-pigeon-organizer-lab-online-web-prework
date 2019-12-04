0d5a12c36c55dc9b1f03355bf9da632c0c18c739
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 20:48:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,19 @@
-def pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
+def def nyc_pigeon_organizer(pigeon_data)
+  new_hash = {}
+  pigeon_data.each do |attribute, data|
+    data.each do |key, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+        if !new_hash[name].has_key?(attribute)
+          new_hash[name][attribute] = []
+        end
+        if !new_hash[name][attribute].include?(key)
+          new_hash[name][attribute] << key.to_s
+        end
+      end
+    end
+  end
+  new_hash
+end
